76fe783c0d6370d01d6717cc00188b96b9f1d592
aabbcc <iiooiio@hotmail.com>
2019-01-22 20:58:28 +0800

    fix some bug

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -1,142 +1,138 @@
 package snake;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
-
-//貪食蛇
+ 
+ 
 public class Snake {
-	private Node head = null;//頭
-	private Node tail = null;//尾
-	private int size = 0;//一開始0節
-
-	
-	private Node n = new Node(20,30, Dir.L);
-		
-	public Snake() {
-		head = n;
-		tail = n;
-		size = 1;
-	}
-	
-	public void addToTail() {//變長
-		//判斷方向: 如蛇往左 尾巴就要加在右邊
-		Node node = null;
-		switch(tail.dir) {//尾巴的方向
-			case L:
-				node = new Node(tail.row,tail.col+1, tail.dir);
-				break;
-			case U:
-				node = new Node(tail.row+1,tail.col, tail.dir);
-				break;
-			case R:
-				node = new Node(tail.row,tail.col-1, tail.dir);
-				break;
-			case D:
-				node = new Node(tail.row-1,tail.col, tail.dir);
-				break;
-		}
-		//雙向鍊表
-		tail.next = node;
-		node.prev = tail;
-		tail = node;
-		size++;
-	}
-	
-	public void addToHead() {//變長(從頭部)
-		//判斷方向: 如蛇往左 尾巴就要加在右邊
-		Node node = null;
-		switch(head.dir) {//尾巴的方向
-			case L:
-				node = new Node(head.row,head.col-1, head.dir);
-				break;
-			case U:
-				node = new Node(head.row-1,head.col, head.dir);
-				break;
-			case R:
-				node = new Node(head.row,head.col+1, head.dir);
-				break;
-			case D:
-				node = new Node(head.row+1,head.col, head.dir);
-				break;
-		}
-		//雙向鍊表
-		node.next = head;
-		head.prev = node; 
-		head = node;
-		size++;
-	}
-	
-	//把整條蛇畫出來
-	public void draw(Graphics g) {
-		if(size <=0) return;
-		for(Node n = head; n != null; n=n.next ) {
-			n.draw(g);
-		}
-		move();//畫完移動到下一節
-	}
-	
-	private void move() {//移動 覆蓋前一節的位置即可(頭加一節 尾去一節 即為移動)
-		addToHead();
-		deleteFromTail();//去掉尾部
-	}
-
-	private void deleteFromTail() {
-		if(size == 0) return;
-		
-		//找到尾巴的前一節 並把那節的next改為空值
-		tail = tail.prev;
-		tail.prev = null;
-		
-	}
-
-	private class Node{//每節
-		int w = Yard.BLOCK_SIZE;//每節的寬等於地圖的寬
-		int h = Yard.BLOCK_SIZE;//每節的高等於地圖的寬
-		int row, col;//所在位置
-		Dir dir = Dir.L;//一開始就給他朝左走
-		Node next = null;//下一節是誰(一開始為空)
-		Node prev = null;//前一節是誰(一開始為空)
-		
-		Node(int row, int col, Dir dir) {
-			this.row = row;
-			this.col = col;
-			this.dir = dir;
-		}
-		
-		void draw(Graphics g) {//把自己畫出來
-			Color c = g.getColor();
-			g.setColor(Color.BLACK);
-			
-			//畫矩型 左上角的點就是所在位置乘以地圖的格子寬(高)
-			g.fillRect(Yard.BLOCK_SIZE*col, Yard.BLOCK_SIZE*row, w, h);
-			g.setColor(c);
-		}
-	}
-
-	public void keyPressed(KeyEvent e) {
-		
-		int key = e.getKeyCode();
-		switch(key) {
-		//蛇的方向就是蛇頭的方向
-		case KeyEvent.VK_LEFT:
-			head.dir = Dir.L;
-			break;
-		case KeyEvent.VK_UP:
-			head.dir = Dir.U;
-			break;
-		case KeyEvent.VK_RIGHT:
-			head.dir = Dir.R;
-			break;
-		case KeyEvent.VK_DOWN:
-			head.dir = Dir.D;
-			break;
-		}
-	}
-	
-	
-	
-
-	
-	
-	
+     
+    Node head = null;
+    Node tail = null;
+    int size = 0;
+    Node n = new Node(20, 30, Dir.L);
+     
+    public  Snake() {
+        head = n;
+        tail = n;
+        size = 1;
+    }
+     
+    public class Node {
+         
+        int w = Yard.BLOCK_SIZE;
+        int h = Yard.BLOCK_SIZE;
+        int row, col;
+        Dir dir = Dir.L;
+        Node next = null;
+        Node prev = null;
+         
+        Node(int row, int col, Dir dir) {
+            this.row = row;
+            this.col = col;
+            this.dir = dir;
+        }
+         
+        public void draw(Graphics g) {
+             
+            Color c = g.getColor();
+            g.setColor(Color.BLACK);
+            g.fillRect(Yard.BLOCK_SIZE * col, Yard.BLOCK_SIZE * row, w, h);
+            g.setColor(c);
+             
+        }
+         
+     
+    } 
+     
+    public void addToTail() {
+         
+        Node node = null;
+         
+        switch(tail.dir) {
+            case L:
+                node  = new Node(tail.row, tail.col + 1, tail.dir);
+                break;
+            case U:
+                node  = new Node(tail.row + 1, tail.col, tail.dir);
+                break;
+            case R:
+                node  = new Node(tail.row, tail.col - 1, tail.dir);
+                break;
+            case D:
+                node  = new Node(tail.row - 1, tail.col, tail.dir);
+                break;
+        }
+         
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        size++;
+         
+    }
+     
+    public void addToHead() {
+         
+        Node node = null;
+         
+        switch(head.dir) {
+            case L:
+                node  = new Node(head.row, head.col - 1, head.dir);
+                break;
+            case U:
+                node  = new Node(head.row - 1, head.col, head.dir);
+                break;
+            case R:
+                node  = new Node(head.row, head.col + 1, head.dir);
+                break;
+            case D:
+                node  = new Node(head.row + 1, head.col, head.dir);
+                break;
+        }
+         
+        node.next = head;
+        head.prev = node;
+        head = node;
+        size++;
+         
+    }
+     
+    public void draw(Graphics g) {
+        if (size <= 0)     return;
+        move();
+        for(Node n = head; n != null; n = n.next) {
+            n.draw(g);
+        }
+ 
+    }
+ 
+    private void move() {
+ 
+        addToHead();
+        deleteFromTail();
+    }
+ 
+    private void deleteFromTail() {
+        if(size ==0) return;
+        tail = tail.prev;
+        tail.next = null;
+    }
+ 
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch(key) {
+        case  KeyEvent.VK_LEFT:
+            head.dir = Dir.L;
+            break;
+        case  KeyEvent.VK_UP:
+            head.dir = Dir.U;
+            break;
+        case  KeyEvent.VK_RIGHT:
+            head.dir = Dir.R;
+            break;
+        case  KeyEvent.VK_DOWN:
+            head.dir = Dir.D;
+            break;
+        }
+    }
 }
--- src/snake/Yard.java
+++ src/snake/Yard.java
@@ -1,6 +1,6 @@
 package snake;
+//Yard.java 
 import java.awt.Color;
-//貪食蛇的地圖(後院)
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -10,101 +10,90 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-
 public class Yard extends Frame {
-	
 
+  public static final int ROWS = 50;
+  public static final int COLS = 50;
+  public static final int BLOCK_SIZE = 10;
+   
+  private boolean gameOver = false;
+   
+  Snake s = new Snake();
+   
+  Image offScreenImage = null;
+   
+  public void lauch() {
+      this.setLocation(300, 300);
+      this.setSize(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+      this.addWindowListener(new WindowAdapter() {
 
-	public static final int ROWS =  50;//橫的格子
-	public static final int COLS = 50;//縱的格子
-	public static final int BLOCK_SIZE = 15;//每格的寬高
-	
-	Snake s = new Snake();
-	
-	Image offScreenImage = null;
-	
-	public void launch() {//畫圖主method
-		this.setLocation(300,300);
-		this.setSize(COLS*BLOCK_SIZE,ROWS* BLOCK_SIZE);
-		
-		
-		
-		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame寫法
-		this.addWindowListener(new WindowAdapter() {//Frame寫法
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
-		this.setVisible(true);
-		
-		new Thread(new PaintThread()).start();//調用thread並啟動
-		this.addKeyListener(new KeyMonitor());
-	}
+          public void windowClosing(WindowEvent arg0) {
+              System.exit(0);
+          }
+           
+      });           
+      this.setVisible(true);
+       
+      new Thread(new PaintThread()).start();
+      addKeyListener(new KeyMonitor()); 
+  }
+   
+   
+  public static void main(String[] args) {
+       
+      new Yard().lauch();
+       
+  }
+   
+  public void paint(Graphics g) {
+      Color c = g.getColor();
+      g.setColor(Color.GRAY);
+      g.fillRect(0, 0, COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+      g.setColor(Color.DARK_GRAY);
+      for(int i=1; i<ROWS; i++) {
+          g.drawLine(0, BLOCK_SIZE * i, COLS * BLOCK_SIZE, BLOCK_SIZE * i);
+      }
+      for(int i=1; i<COLS; i++) {
+          g.drawLine(BLOCK_SIZE * i, 0, BLOCK_SIZE * i, BLOCK_SIZE * ROWS);
+      }
+      g.setColor(c);
+       
+      s.draw(g);
+       
+  }
+   
 
-	
-	public static void main(String[] args) {
-		new Yard().launch();
-	}
-	
-	//畫背景
-	public void paint(java.awt.Graphics g) {
-		// TODO Auto-generated method stub
-		Color c = g.getColor();
-		g.setColor(Color.GRAY);
-		g.fillRect(0,0,COLS*BLOCK_SIZE,ROWS* BLOCK_SIZE);
-		g.setColor(Color.DARK_GRAY);
-		//畫出橫線
-		for(int i=1; i<ROWS; i++) {
-			g.drawLine(0, BLOCK_SIZE *i , ROWS*BLOCK_SIZE, BLOCK_SIZE *i);
-		}
-		
-		//畫出直線
-		for(int i=1; i<COLS; i++) {
-			g.drawLine(BLOCK_SIZE *i, 0 , BLOCK_SIZE *i, ROWS* BLOCK_SIZE);
-		}
-		
-		g.setColor(c);
-		
-		s.draw(g);//畫蛇
-	}
-	
-	@Override
-	public void update(Graphics g) {//覆寫update方法 判斷如果圖是一樣的就不repaint
-		if(offScreenImage == null) {
-			offScreenImage = this.createImage(COLS*BLOCK_SIZE,ROWS* BLOCK_SIZE);
-		}
-		Graphics gOff = offScreenImage.getGraphics();
-		paint(gOff);
-		g.drawImage(offScreenImage, 0,0, null);
-	}
-	
-	//讓蛇移動
-	//建立thread類
-	private class PaintThread implements Runnable{
+  public void update(Graphics g) {
+      if(offScreenImage == null) {
+          offScreenImage = this.createImage(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+      }
+      Graphics gOff = offScreenImage.getGraphics();
+      paint(gOff);
+      g.drawImage(offScreenImage, 0, 0, null);
+  }
+   
+  private class PaintThread implements Runnable{
 
-		@Override
-		public void run() {
-			while(true) {
-				repaint();//重繪
-				try {
-					Thread.sleep(50);//50ms
-					
-				}catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
-	//監聽鍵盤
-	private class KeyMonitor extends KeyAdapter{
-		
-		public void keyPressed(KeyEvent e) {
-			s.keyPressed(e);
-		}
+      public void run() {
+          while(true) {
+              repaint();
+              try {
+                  Thread.sleep(50);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+           
+      }
+       
+  }
+   
+  private class KeyMonitor extends KeyAdapter{
 
-
-	}
-	
+      public void keyPressed(KeyEvent e) {
+          s.keyPressed(e); 
+      }
+       
+  }
 
 }
